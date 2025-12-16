bb10bf3de924348a83e1c6e40dbcc955431e922a
QuocTruong015 <ngocquoc150604@gmail.com>
2025-12-16 15:18:33 +0700

    send photo and video

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
     senderId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
     receiverId:{type:mongoose.Schema.Types.ObjectId,ref:"User", default:null},
-    message:{type:String,required:true},
+    message:{type:String,default:""},
+    image:{type:String,default:null},
+    video:{type:String,default:null},
     conversationId: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation", required: true },
 },{timestamps:true})
 
